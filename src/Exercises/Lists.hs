926aa0ac34c61069a637a6e01a8183cec0bb3570
Marc-Daniel Ortega <globulon@gmail.com>
2019-06-08 19:56:52 +0100

    Add anamorphism

--- src/Exercises/Lists.hs
+++ src/Exercises/Lists.hs
@@ -1,6 +1,15 @@
-module Exercises.Lists(reverse') where
+module Exercises.Lists(reverse', count') where
 
 import Sorting.Catamorphism(Algebra(..), Catamorphism(..))
+import Sorting.Anamorphism(Anamorphism(..), CoAlgebra(..))
 
 reverse' :: [a] -> [a]
 reverse' = cata (Algebra [] (\x y -> y ++ [x]))
+
+count' :: (Num a, Eq a, Ord a) => a -> [a]
+count'  = ana (CoAlgebra unfoldCount)
+
+unfoldCount :: (Num a, Eq a, Ord a) => a -> Either () (a, a)
+unfoldCount n
+  | n <= 0    = Left ()
+  | otherwise = Right (n, n - 1)
--- test/Exercises/ListsSpec.hs
+++ test/Exercises/ListsSpec.hs
@@ -1,13 +1,28 @@
 module Exercises.ListsSpec(spec) where
 
-import Exercises.Lists(reverse')
+import Exercises.Lists(reverse', count')
 import Test.Hspec
 import Test.QuickCheck
 
 
 spec :: Spec
-spec = describe "reverse'" $ do
+spec = do
+  spec_reverse
+  spec_count
+
+spec_reverse :: Spec
+spec_reverse = describe "reverse'" $ do
   it "should conserve empty list" $
     (reverse' []::[Int]) `shouldBe` []
   it "should reverse empty list" $
     property (\xs -> (reverse' xs::[Int]) == reverse xs)
+
+spec_count :: Spec
+spec_count = describe "count should" $ do
+  it "conserve 0" $
+    count' 0 `shouldBe` []
+  it "generate reverse list" $
+    property checkReverse
+
+checkReverse :: Int -> Bool
+checkReverse n = count' n == reverse' [1 .. n]
