527f78e1e57b61e5da2d6711e9560b0a9478257b
Marc-Daniel Ortega <globulon@gmail.com>
2019-06-30 00:52:57 +0100

    Add more optic typeclasses

--- src/Category/Optics.hs
+++ src/Category/Optics.hs
@@ -2,8 +2,23 @@ module Category.Optics where
 
 data Lens a b s t = Lens { view :: s -> a , update :: (s, b) -> t }
 
+data Prism a b s t = Prism { match :: s -> Either t a, build :: b  -> t }
+
+data Adapter a b s t = Adapter { from :: s -> a, to :: b -> t }
+
 sndLens :: Lens a b (c, a) (c, b)
 sndLens = Lens vw up where
   vw             = snd
   up ((x, y), z) = (x, z)
 
+the :: Prism a b (Maybe a) (Maybe b)
+the = Prism mt bd
+  where
+    mt (Just a) = Right a
+    mt _ = Left Nothing
+    bd = Just
+
+nested :: Adapter (a, b, c) (a', b', c') ((a, b), c) ((a', b'), c')
+nested = Adapter from to where
+  from ((a, b), c) = (a, b, c)
+  to (a, b, c) = ((a, b), c)
--- src/Category/Profunctor.hs
+++ src/Category/Profunctor.hs
@@ -12,5 +12,22 @@ class Profunctor p where
   {-# INLINE rmap #-}
   {-# MINIMAL dimap | (lmap, rmap)  #-}
 
+class Profunctor p => Cartesian p where
+  second :: p a b -> p (c, a) (c, b)
+  {-# MINIMAL second #-}
+
+class Profunctor p => CoCartesian p where
+  right :: p a b -> p (Either c a) (Either c b)
+  {-# MINIMAL right #-}
+
 instance Profunctor (->) where
   dimap f h g = h . g . f
+  {-# INLINE dimap #-}
+
+instance Cartesian (->) where
+  second = fmap
+  {-# INLINE second #-}
+
+instance CoCartesian (->) where
+  right = fmap
+  {-# INLINE right #-}
